89cb464e9c748b484fd5d5971ccf56fa0ea5bd7f
DayanVT <day1s.torr35@gmail.com>
2023-09-21 23:49:36 -0600

    Programa del 1 al 6

--- 03EstructurasControl/EstructurasControl.java
+++ 03EstructurasControl/EstructurasControl.java
@@ -45,6 +45,15 @@ class EstructurasControl{
         String nombreproducto;
         String binario = "";
         char letra;
+        int edad; //caso 1
+        double desc = 0; //caso1
+        String respuesta = ""; //caso1
+        int tiposgrado, cant_grado;//caso 3
+        double celsius, kelvin, rankine, fahrenheit;//caso 3
+        int num, positivos = 0;//caso 4
+        int negativos = 0; //caso 4
+        double base, altura, radio, volumen, perimetro, area; //caso 6
+        
 
         do{
             //aqui va todo el programa
@@ -70,6 +79,25 @@ class EstructurasControl{
             //menu
             switch(opcion){
                 case 1:
+                    System.out.println("Ingrese su edad: ");
+				    edad = entrada.nextInt();
+
+		            if(edad >= 65){
+                        desc = 0.4 * 100;
+                        System.out.println("El descuento del abono es: " + desc);
+						}else 
+                        if (edad <= 21){
+                            System.out.println("¿Sus padres son socios?, ¿Si o no?");
+                            respuesta = entrada.next();
+                            if (respuesta.equals( "Si") || respuesta.equals("si")){
+                            desc = 0.45 * 100;
+                            System.out.println("Tiene un descuento del abono de: " + desc);
+                            }else if (respuesta.equals("No") || respuesta.equals("no")){
+                                desc = .25 * 100;
+                                System.out.println("El descuento del abono es de: " + desc);
+                                }
+                        }
+                        
                     break;
                 case 2:
                     //decimal a binario
@@ -94,8 +122,66 @@ class EstructurasControl{
                     System.out.println("El numero binario es: " + binario);
                     break;
                 case 3:
+                    //convertir temperaturas
+                    System.out.println("¿A que tipo de temperatura quiere convertir?");
+                    System.out.println("1.- Fahrenheit ");
+                    System.out.println("2.- Celsius");
+                    System.out.println("3.- Kelvin");
+                    System.out.println("4.- Rankine");
+					tiposgrado = entrada.nextInt();
+					
+                    System.out.println("Ingresar la cantidad de grados: ");
+                    cant_grado = entrada.nextInt();
+					
+						if(tiposgrado == 1) {
+							celsius = (cant_grado - 32)*5/9; 
+							kelvin = (cant_grado - 32)*5/9+273.15;
+							rankine = (cant_grado - 32)+491.67;
+							System.out.println(cant_grado + " La equivalencia en celsius es: " + celsius + " La equivalencia en kelvin es: " + kelvin +" La equivalencia en rankine es: " + rankine);
+						}
+						else{
+							if(tiposgrado == 2){
+								fahrenheit = (cant_grado * ( 9/5 ))+ 32;
+								kelvin = cant_grado+273.15;
+								rankine = (cant_grado * (9/5))+491.67;
+								System.out.println(cant_grado + " La equivalencia en Fahrenheit es: " + fahrenheit + " La equivalencia en kelvin es: " + kelvin +" La equivalencia en rankine es: " + rankine);
+							}
+							else{
+								if (tiposgrado == 3) {
+									celsius = cant_grado-273.15;
+									fahrenheit = (cant_grado - 273.15) * 9/5+32;
+									rankine = (cant_grado - 273.15) * (5/9) + 491.67;
+									System.out.println(cant_grado +" La equivalencia en Fahrenheit es: " + fahrenheit + " La equivalencia en celsius es: " + celsius +" La equivalencia en rankine es: " + rankine);
+								}
+								else{
+									if(tiposgrado == 4) {
+										celsius = (cant_grado * (9/5)) + 491.67;
+										fahrenheit = cant_grado-459.67;
+										kelvin = cant_grado * (5/9);
+										System.out.println(cant_grado +" La equivalencia en Fahrenheit es: " + fahrenheit +" La equivalencia en celsius es: " + celsius +" La equivalencia en kelvin es: " + kelvin);
+									}
+									
+								}
+							}
+							
+						}
                     break;
                 case 4:
+                    System.out.println("¿Cuantos numeros desea ingresar?");
+                    total = entrada.nextInt();
+
+                    for (int i = 1; i <=total; i++){
+                        System.out.println("Ingrese el numero: ");
+                        num = entrada.nextInt();
+
+                        if (num > 0){
+                            positivos++;
+                        }else {
+                            negativos++;
+                        }
+                    }
+                    System.out.println("Numeros positivos en total son: " + positivos);
+                    System.out.println("Numeros negativos en total son: " + negativos);
                     break;
                 case 5:
                     System.out.println("Bienvenido a la tiendita ");
@@ -117,6 +203,62 @@ class EstructurasControl{
                     compra = 0;
                     break;
                 case 6:
+                    System.out.println("Seleccione la figura deseada:");
+                    System.out.println("1- Rectángulo");
+                    System.out.println("2- Triángulo");
+                    System.out.println("3- Esfera");
+                    System.out.println("4- Cilindro");
+                    opcion = entrada.nextInt();
+
+                    switch (opcion) {
+                        case 1:
+                            System.out.print("Ingrese la base del rectángulo: ");
+                            base = entrada.nextDouble();
+                            System.out.print("Ingrese la altura del rectángulo: ");
+                            altura = entrada.nextDouble();
+
+                            area = base * altura;
+                            perimetro = 2 * (base + altura);
+
+                            System.out.println("Área del rectángulo: " + area);
+                            System.out.println("Perímetro del rectángulo: " + perimetro);
+                            break;
+
+                        case 2:
+                            System.out.print("Ingrese la base del triángulo: ");
+                            base = entrada.nextDouble();
+                            System.out.print("Ingrese la altura del triángulo: ");
+                            altura = entrada.nextDouble();
+
+                            area = (base * altura) / 2;
+                            perimetro = base * 3;
+
+                            System.out.println("Área del triángulo: " + area);
+                            System.out.println("Perímetro del triángulo: " + perimetro);
+                            break;
+
+                        case 3:
+                            System.out.print("Ingrese el radio de la esfera: ");
+                            radio = entrada.nextDouble();
+                            volumen = (4.0 / 3.0) * Math.PI * Math.pow(radio, 3);
+
+                            System.out.println("Volumen de la esfera: " + volumen);
+                            break;
+
+                        case 4:
+                            System.out.print("Ingrese el radio de la base del cilindro: ");
+                            radio = entrada.nextDouble();
+                            System.out.print("Ingrese la altura del cilindro: ");
+                            altura = entrada.nextDouble();
+                            volumen = Math.PI * Math.pow(radio, 2) * altura;
+
+                            System.out.println("Volumen del cilindro: " + volumen);
+                            break;
+
+                        default:
+                            System.out.println("Opcion invalida");
+                            break;
+                }
                     break;
                 case 7:
                     break;

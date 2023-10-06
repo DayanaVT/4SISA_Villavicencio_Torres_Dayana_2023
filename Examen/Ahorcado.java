348034757721458f52b8e352ca91322f4a768cd5
DayanVT <day1s.torr35@gmail.com>
2023-10-06 14:35:40 -0600

    Examen

--- Examen/Ahorcado.java
+++ Examen/Ahorcado.java
@@ -20,10 +20,30 @@ class Ahorcado{
 
         letraAdivinada = new char[palabra.length()];
 
+        //pistas 
+        System.out.println("La palabra a adivinar contiene: " + palabra.length() + " letras");
+        System.out.println("La primer letra de la palabra es: " + palabra.charAt(0));
+        System.out.println("La ultima letra de la palabra es: " + palabra.charAt(palabra.length() - 1));
+
         while(intentos > 0){
-            System.out.println("Ingresa ");
+            System.out.println("La palabra es: " + palabra.charAt(0) + " " + palabra.charAt(palabra.length() - 1));
+            System.out.println("Tienes: " + intentos + " intentos");
+            System.out.println("Ingresa una letra: ");
             char letra = entrada.next().charAt(0);
+
+            if (isLetterInWord(palabra, letraAdivinada)) {
+                System.out.println("Letra correcta");
+                puntaje += 10;
+            }else {
+                System.out.println("Letra incorrecta, introduce otra letra");
+                intentos--;
+                puntaje -= 5;
+            }
         }
 
+    }
+
+    private static boolean isLetterInWord(String palabra, char[] letraAdivinada) {
+        return false;
     }
 }

6f4a22633b1e41b24f77ea37b005baedb3430d6f
DayanVT <day1s.torr35@gmail.com>
2023-11-11 10:00:25 -0600

    Paleta color

---
+++ 11Ventanita/src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dayan
+ */
+public class Main {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana miventana = new Ventana();
+        miventana.setVisible(true);
+    }
+    
+}
